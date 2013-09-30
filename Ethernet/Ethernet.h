5823e314358dbe08f7a82d9b49888c492741e75a
Soohwan Kim <suhwan@wiznet.co.kr>
2013-09-30 11:45:58 +0900

    Fixed

--- Ethernet/Ethernet.h
+++ Ethernet/Ethernet.h
@@ -21,14 +21,6 @@ private:
 public:
   static uint8_t _state[MAX_SOCK_NUM];
   static uint16_t _server_port[MAX_SOCK_NUM];
-  // Initialise the Ethernet shield to use the provided MAC address and gain the rest of the
-  // configuration through DHCP.
-  // Returns 0 if the DHCP configuration failed, and 1 if it succeeded
-  int begin(uint8_t *mac_address);
-  void begin(uint8_t *mac_address, IPAddress local_ip);
-  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server);
-  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
-  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
 
 #if defined(WIZ550io_WITH_MACADDRESS)
   // Initialize function when use the ioShield serise (included WIZ550io)
@@ -40,6 +32,16 @@ public:
   void begin(IPAddress local_ip, IPAddress dns_server);
   void begin(IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
   void begin(IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
+#else
+  // Initialize the Ethernet shield to use the provided MAC address and gain the rest of the
+  // configuration through DHCP.
+  // Returns 0 if the DHCP configuration failed, and 1 if it succeeded
+  int begin(uint8_t *mac_address);
+  void begin(uint8_t *mac_address, IPAddress local_ip);
+  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server);
+  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
+  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
+
 #endif
   
   int maintain();

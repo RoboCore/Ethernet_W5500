ae731f98a196c05725de1cd8743d3fb4caf9bfea
Arjen Hiemstra <arjenh@me.com>
2014-04-12 00:28:01 +0200

    1284P Support

--- Arduino IDE 1.0.x/Ethernet/utility/w5100.h
+++ Arduino IDE 1.0.x/Ethernet/utility/w5100.h
@@ -338,7 +338,7 @@ private:
 
 private:
     
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284P__)
   inline static void initSS()    { DDRB  |=  _BV(4); };
   inline static void setSS()     { PORTB &= ~_BV(4); };
   inline static void resetSS()   { PORTB |=  _BV(4); };
